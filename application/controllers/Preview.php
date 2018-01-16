384cad49f078958cf7c2f1f7346d5582bff25b64
王洪军 <hongjun6@staff.weibo.com>
2018-01-16 16:16:30 +0800

    fix bug

--- application/controllers/Preview.php
+++ application/controllers/Preview.php
@@ -114,7 +114,7 @@ class PreviewController extends Yaf_Controller_Abstract
             }
         }
 
-        $types = [1 => [1], 2 => [2, 3, 4], 3 => [5, 6], 4 => []];
+        $types = [1 => [1, 2, 3, 4, 5, 6], 2 => []];
 
 
         $html_article = [];
